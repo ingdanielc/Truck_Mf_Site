4b3cc694830621ac6871fb153d95df775970ffe1
Daniel Andrés Castro Solis (GTK) <dsolis@gattaca.co>
2026-02-24 15:27:43 -0500

    Update: Cambio en formato de fechas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
-    HTTP_INTERCEPTORS,
-    provideHttpClient,
-    withInterceptorsFromDi,
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
 } from '@angular/common/http';
 import { APP_BASE_HREF, registerLocaleData } from '@angular/common';
 import localeEsCo from '@angular/common/locales/es-CO';
@@ -16,17 +20,18 @@ import { HttpHeadersInterceptor } from './services/utils/http-headers.service';
 registerLocaleData(localeEsCo);
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes),
-        provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi()),
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: HttpHeadersInterceptor,
-            multi: true,
-        },
-        CookieService,
-        { provide: APP_BASE_HREF, useValue: '/truck' },
-    ],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+    CookieService,
+    { provide: APP_BASE_HREF, useValue: '/truck' },
+    { provide: LOCALE_ID, useValue: 'es-CO' },
+  ],
 };
